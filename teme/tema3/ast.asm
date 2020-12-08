d1fefdff0ec4df24dead3926a2b68463479900f4
student <d.raduandrei@gmail.com>
2020-12-08 23:55:31 +0200

    wrong move

--- teme/tema3/ast.asm
+++ teme/tema3/ast.asm
@@ -17,55 +17,55 @@ iocla_atoi:
         push    esi
         push    ebx
         xor     ecx, ecx
-        mov     eax, DWORD PTR [esp+12]
+        mov     eax, DWORD [esp + 12]
         mov     esi, 1
-        movsx   edx, BYTE PTR [eax]
-        cmp     dl, 45
-        jne     .L2iocla_atoi
-        movsx   edx, BYTE PTR [eax+1]
+        movsx   edx, BYTE [eax]
+        cmp     dl, 45 ; check if string[0] == '-'
+        jne     atoi_negative_number
+        movsx   edx, BYTE [eax + 1]
         mov     esi, -1
         mov     ecx, 1
-.L2iocla_atoi:
-        lea     ebx, [edx-48]
-        lea     ecx, [eax+1+ecx]
+atoi_negative_number:
+        lea     ebx, [edx - 48]
+        lea     ecx, [eax + 1 + ecx]
         xor     eax, eax
-        cmp     bl, 9
-        ja      .L1iocla_atoi
-.L3iocla_atoi:
-        lea     ebx, [eax+eax*4]
+        cmp     bl, 9 ; check if number
+        ja      iocla_atoi_final
+atoi_string_iterator:
+        lea     ebx, [eax + eax * 4]
         add     ecx, 1
-        lea     eax, [edx-48+ebx*2]
-        movsx   edx, BYTE PTR [ecx-1]
-        lea     ebx, [edx-48]
-        cmp     bl, 9
-        jbe     .L3iocla_atoi
+        lea     eax, [edx - 48 + ebx * 2]
+        movsx   edx, BYTE [ecx - 1]
+        lea     ebx, [edx - 48]
+        cmp     bl, 9 ; check if number
+        jbe     atoi_string_iterator
         imul    eax, esi
-.L1iocla_atoi:
+iocla_atoi_final:
         pop     ebx
         pop     esi
         ret
 
-add_tree:
+add_node:
         push    ebp
         push    edi
         push    esi
         push    ebx
         sub     esp, 12
-        mov     edi, DWORD [esp+32]
-        mov     esi, DWORD [esp+36]
-.L9:
+        mov     edi, DWORD [esp + 32]
+        mov     esi, DWORD [esp + 36]
+start_algo_add_node:
         movzx   edx, BYTE [esi]
         cmp     dl, 32
-        jne     .L2
-        movzx   edx, BYTE [esi+1]
+        jne     start_while_true
+        movzx   edx, BYTE [esi + 1]
         add     esi, 1
-.L2:
+start_while_true:
         test    dl, dl
-        je      .L3
+        je      finish_iterator
         mov     ebp, DWORD [edi]
         mov     ebx, 1
         test    ebp, ebp
-        jne     .L5
+        jne     check_if_number
         sub     esp, 12
         mov     ebx, 1
         push    12
@@ -75,12 +75,12 @@ add_tree:
         add     esp, 16
         sub     eax, 48
         cmp     al, 9
-        jbe     .L7
-.L6:
+        jbe     iterate_string_check_number
+create_node_in_memory:
         sub     esp, 8
         push    ebx
         push    esi
-        call    strndup
+        call    strndup ; duplicate the number from the string
         mov     DWORD [ebp+4], 0
         mov     DWORD [ebp+0], eax
         add     esp, 16
@@ -88,56 +88,56 @@ add_tree:
         mov     DWORD [edi], ebp
         movzx   edx, BYTE [esi]
         test    dl, dl
-        jne     .L5
-.L3:
+        jne     check_if_number
+finish_iterator:
         xor     eax, eax
-.L1:
+add_node_return:
         add     esp, 12
         pop     ebx
         pop     esi
         pop     edi
         pop     ebp
         ret
-.L7:
+iterate_string_check_number:
         add     ebx, 1
         movzx   eax, BYTE [esi+ebx]
         lea     edx, [eax-48]
         cmp     dl, 9
-        ja      .L6
+        ja      create_node_in_memory
         add     ebx, 1
         movzx   eax, BYTE [esi+ebx]
         lea     edx, [eax-48]
         cmp     dl, 9
-        jbe     .L7
-        jmp     .L6
-.L5:
+        jbe     iterate_string_check_number
+        jmp     create_node_in_memory
+check_if_number:
         sub     edx, 48
         lea     eax, [esi+ebx]
         cmp     dl, 9
-        jbe     .L1
+        jbe     finish_iterator
         movzx   edx, BYTE [esi+1]
         sub     edx, 48
         cmp     dl, 9
-        jbe     .L1
+        jbe     finish_iterator
         sub     esp, 8
         add     ebp, 4
         push    eax
         push    ebp
-        call    add_tree
+        call    add_node ; add left child 
         mov     edi, DWORD [edi]
         lea     esi, [eax+ebx]
         add     esp, 16
         add     edi, 8
-        jmp     .L9
+        jmp     start_algo_add_node ; add rigth child
 
 
 create_tree:
         sub     esp, 36
-        mov     DWORD [esp+20], 0
-        push    DWORD [esp+40]
-        lea     eax, [esp+24]
+        mov     DWORD [esp + 20], 0
+        push    DWORD [esp + 40]
+        lea     eax, [esp + 24]
         push    eax
-        call    add_tree
-        mov     eax, DWORD [esp+28]
+        call    add_node
+        mov     eax, DWORD [esp + 28]
         add     esp, 44
         ret
